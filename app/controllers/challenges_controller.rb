46d6c885a58744db5243e1f62dcbf09352ce8c70
henry2992 <henry2992@hotmail.com>
2016-08-26 07:46:58 -0500

    check without Redis

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -14,15 +14,15 @@ class ChallengesController < ApplicationController
       @challenges = Challenge.all
     end
 
-    @leader_board = Player.leaderboard.revrange(0, 9)
+    # @leader_board = Player.leaderboard.revrange(0, 9)
 
-    @players = []
+    # @players = []
 
-    @leader_board.each do |player|
-      @players << Player.find_by_id(player)
-    end
+    # @leader_board.each do |player|
+    #   @players << Player.find_by_id(player)
+    # end
 
-    @player_rank = current_player.my_rank
+    # @player_rank = current_player.my_rank
   	
   end
 
