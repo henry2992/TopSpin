38115f6068131f06f687ea79b4f025cbc9a24214
henry2992 <henry2992@hotmail.com>
2016-08-27 14:55:17 -0500

    Add Redis To go Heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis::Namespace.new("my_app", :redis => Redis.new)
+# $redis = Redis::Namespace.new("my_app", :redis => Redis.new)
+
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:url => uri)
