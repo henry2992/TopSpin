5bad2867708c6be332ffa4e50e58dcedde4197b2
henry2992 <henry2992@hotmail.com>
2016-10-31 23:32:58 -0500

    Fb

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,13 +10,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # set_flash_message(:notice, :success, :kind => @player.first_name ) if is_navigational_format?
 
 
-      if ((Date.today.mjd - @player.last_sign_in_at.to_date.mjd) < 0 )
-        message = 'Welcome back! It’s been 0 days since you last completed an activity. Let’s go play some tennis!'
-      else 
-        message = 'Welcome back! It’s been ' + (Date.today.mjd - current_player.last_sign_in_at.to_date.mjd ) + ' days since you last completed an activity. Let’s go play some tennis!'
-      end 
+      # if ((Date.today.mjd - @player.last_sign_in_at.to_date.mjd) < 0 )
+      #   message = 'Welcome back! It’s been 0 days since you last completed an activity. Let’s go play some tennis!'
+      # else 
+      #   message = 'Welcome back! It’s been ' + (Date.today.mjd - current_player.last_sign_in_at.to_date.mjd ) + ' days since you last completed an activity. Let’s go play some tennis!'
+      # end 
 
-      flash[:notice] = message
+      # flash[:notice] = message
       
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"].except('extra')
