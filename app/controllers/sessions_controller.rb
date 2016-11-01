0f00618cf748709307a37583bc283572d0f0ef29
henry2992 <henry2992@hotmail.com>
2016-10-31 23:57:02 -0500

    fix email signin flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < Devise::SessionsController
     if ((Date.today.mjd - current_player.last_sign_in_at.to_date.mjd) < 0 )
       message = 'Welcome back! It’s been 0 days since you last completed an activity. Let’s go play some tennis!'
     else 
-      message = 'Welcome back! It’s been ' + (Date.today.mjd - current_player.last_sign_in_at.to_date.mjd ) + ' days since you last completed an activity. Let’s go play some tennis!'
+      message = 'Welcome back! It’s been ' + (Date.today.mjd - current_player.last_sign_in_at.to_date.mjd ).to_s + ' days since you last completed an activity. Let’s go play some tennis!'
     end 
 
     flash[:notice] = message
