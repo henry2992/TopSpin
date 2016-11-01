e75c5d4781acf1cdf08c5630645feb556086d35a
henry2992 <henry2992@hotmail.com>
2016-10-31 23:50:48 -0500

    fix fb

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -13,8 +13,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if ((Date.today.mjd - @player.last_sign_in_at.to_date.mjd) < 0 )
         message = 'Welcome back! It’s been 0 days since you last completed an activity. Let’s go play some tennis!'
       else 
-        message = 'here'
-        # message = 'Welcome back! It’s been ' + (Date.today.mjd - @player.last_sign_in_at.to_date.mjd ) + ' days since you last completed an activity. Let’s go play some tennis!'
+
+        message = 'Welcome back! It’s been ' + (Date.today.mjd - @player.last_sign_in_at.to_date.mjd ) + ' days since you last completed an activity. Let’s go play some tennis!'
       end 
 
 
